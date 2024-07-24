c05bd5733283bac9c781153dea8e3a4f11b000ae
Larissa Cavalcante <112430561+larissa-sc@users.noreply.github.com>
2024-07-24 20:57:16 -0300

    Delete Projetos AMAS directory

--- Projetos AMAS/script.js
+++
@@ -1,8 +0,0 @@
-// Add event listeners to navigation links
-document.querySelectorAll('nav a').forEach(link => {
-    link.addEventListener('click', event => {
-        event.preventDefault();
-        const target = document.querySelector(link.getAttribute('href'));
-        target.scrollIntoView({ behavior: 'smooth' });
-    });
-});
