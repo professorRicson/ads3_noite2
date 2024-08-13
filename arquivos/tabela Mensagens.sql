23dba010bb36e12bf6a7ddf4ea831ea72357dec8
jadsonj <jadsonj486@gmail.com>
2024-08-13 09:59:31 -0300

    Atualização

---
+++ arquivos/tabela Mensagens.sql
@@ -0,0 +1,8 @@
+Create Table Mensagens(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    Nome Varchar (50),
+    Email Varchar (100),
+    Telefone Varchar(70),
+    Cidade Varchar (80),
+    Mensagens text
+);
